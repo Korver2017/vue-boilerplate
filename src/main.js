0a611eee30fa3a74c01973a91cb05e8032e59870
Korver Huang <korver2017@gmail.com>
2019-08-05 18:34:46 +0800

    Set up main.js & store

--- src/main.js
+++ src/main.js
@@ -1,12 +1,48 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 
-Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  /**
+   *
+   * Import from vendors
+   *
+   */
+
+  // Vue
+  import Vue from 'vue';
+
+  // Bootstrap
+  import 'bootstrap/dist/css/bootstrap.css';
+  import 'bootstrap/dist/js/bootstrap.js';
+
+
+  /**
+   *
+   * Import from files
+   *
+   */
+
+  // Script
+  import app from '@/app.vue';
+  import router from '@/router';
+  import api from '@/vendors/axios/loader.js';
+  import store from '@/store';
+
+
+  /**
+  *
+  * Vue global & config & plugin
+  *
+  */
+  Vue.config.productionTip = false;
+  Vue.prototype.$api = api;
+
+
+  /**
+  *
+  * Vue instance
+  *
+  */
+  new Vue({
+    router,
+    store,
+    render: h => h(app)
+  }).$mount('#app')
